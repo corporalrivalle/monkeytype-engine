80f549c375e68d8c618365f82766461e09896216
Derek Zhang <58023992+corporalrivalle@users.noreply.github.com>
2022-11-17 18:03:49 -0800

    update 0.1
    engine v1.0 created
    engine v2.0 under construction

---
+++ typecheat.py
@@ -0,0 +1,9 @@
+import keyboard as key
+import time
+
+typestring = "from call few in want possible system come could by eye up ask great and end stand even she still that much begin place up about we great might high well back little want still only consider say end come should there great very this would back by"
+def instatype():
+    time.sleep(1)
+    key.write(typestring)
+
+instatype()
